06cda30c9b8a9842d818ea8c67ee59642dbb405f
= <Chershembeev@complete.ru>
2019-07-29 17:04:57 +0300

    springBoot

---
+++ src/main/java/com/example/homework01_4/demo/service/csvParser/Parser.java
@@ -0,0 +1,14 @@
+package com.example.homework01_4.demo.service.csvParser;
+
+import java.util.Map;
+
+/**
+ * Created by Chershembeev_AE
+ * Date: 09.07.2019
+ * Time: 17:05.
+ */
+
+public interface Parser {
+
+    Map<String,String>getAllQuestions(String fileName);
+}
---
+++ src/main/java/com/example/homework01_4/demo/service/csvParser/ParserImpl.java
@@ -0,0 +1,44 @@
+package com.example.homework01_4.demo.service.csvParser;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Chershembeev_AE
+ * Date: 09.07.2019
+ * Time: 17:05.
+ */
+
+@Component
+public class ParserImpl implements Parser {
+    private static final Logger LOGGER = Logger.getLogger(ParserImpl.class.getName());
+
+
+    @Override
+    public Map<String, String> getAllQuestions(String fileName) {
+
+        Map<String, String> questions = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader( new InputStreamReader(getClass().getResourceAsStream("/" + fileName)))) {
+
+            while (reader.ready()) {
+                String line;
+                line = reader.readLine();
+                String[] arr = line.split(",");
+                questions.put(arr[0], arr[1]);
+
+            }
+        } catch (FileNotFoundException e) {
+            LOGGER.error(e.getMessage() + " " + fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return questions;
+    }
+}
